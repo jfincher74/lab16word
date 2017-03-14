10f83f30d6a37eb69848826a66a5f42299904d64
Ashton Albertson <ashtonalbertson@gmail.com>
2017-03-14 19:56:04 -0400

    adding wordModule

--- scripts/module/module.js
+++ scripts/module/module.js
@@ -1,10 +1,11 @@
+var wordModule = angular.module("wordModule", []);
+
 var places = [
-	"Detroit", "Denver", "Atlanta", "Dallas", "Birmingham", "Houston", 
+	"Detroit", "Denver", "Atlanta", "Dallas", "Birmingham", "Houston",
 	"San Francisco", "Los Angelas", "Phenoix", "Seattle", "Portland",
-	"Kansas City", "Saint Louis", "New Orleans", "Chicago", "Indianapolis", 
-	"Lexington", "Louisville", "New York", "Memphis", "Jackson", "Madison", 
-	"Minneapolis", "Savnnah", "Tampa Bay", "Miami", "Las Vegas", "Billings", 
-	"Baltimore", "Philedelphia", "Buffalo", "Raleigh", "Charleston", "Nashville", 
+	"Kansas City", "Saint Louis", "New Orleans", "Chicago", "Indianapolis",
+	"Lexington", "Louisville", "New York", "Memphis", "Jackson", "Madison",
+	"Minneapolis", "Savnnah", "Tampa Bay", "Miami", "Las Vegas", "Billings",
+	"Baltimore", "Philedelphia", "Buffalo", "Raleigh", "Charleston", "Nashville",
 	"Cincinnati", "Cleveland", "Pittsburgh", "Providence", "Austin", "Washington D.C.", "Huntsville"
 ]
-
