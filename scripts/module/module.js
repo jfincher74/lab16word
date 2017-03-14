e44999f772d9d98829d0997da1a7391fe9082ea7
Jay Fincher <fincher_jay@yahoo.com>
2017-03-14 19:47:25 -0400

    updated Module

---
+++ scripts/module/module.js
@@ -0,0 +1,10 @@
+var places = [
+	"Detroit", "Denver", "Atlanta", "Dallas", "Birmingham", "Houston", 
+	"San Francisco", "Los Angelas", "Phenoix", "Seattle", "Portland",
+	"Kansas City", "Saint Louis", "New Orleans", "Chicago", "Indianapolis", 
+	"Lexington", "Louisville", "New York", "Memphis", "Jackson", "Madison", 
+	"Minneapolis", "Savnnah", "Tampa Bay", "Miami", "Las Vegas", "Billings", 
+	"Baltimore", "Philedelphia", "Buffalo", "Raleigh", "Charleston", "Nashville", 
+	"Cincinnati", "Cleveland", "Pittsburgh", "Providence", "Austin", "Washington D.C.", "Huntsville"
+]
+
