0f8db21fc9c5191c5f87b1ca3141b8c893318534
Ashton Albertson <ashtonalbertson@gmail.com>
2017-03-14 21:27:22 -0400

    adding random function

--- scripts/module/module.js
+++ scripts/module/module.js
@@ -13,11 +13,27 @@ app.controller("wordController", function($scope){
   	"Cincinnati", "Cleveland", "Pittsburgh", "Providence", "Austin", "Washington D.C.", "Huntsville"
   ];
 
-  $scope.showWord = function(places, $index) {
-    var city = $scope.places.splice($index, 1);
-    console.log(city);
-  }
+  $scope.showWord = function(places){
+
+  var rand = $scope.places[Math.floor(Math.random() * $scope.places.length)];
+
+  console.log(rand);
+};
 });
+
+  // $scope.showWord = function(places, $index) {
+  //   var city = $scope.places.splice($index, 1);
+  //   console.log(city);
+  // }
+
+  // $scope.showWord = function(){
+  //   $index = Math.random() * [40]
+  // }
+
+
+// $scope.getNumber = function() {
+//              $scope.num = ((Math.random() * [40]));
+//          };
 
 
 // $scope.removeItem = function(letsRemove, $index){
