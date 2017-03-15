8c66bc436da9471b9bfaf79a3628ecd372b8817c
Ashton Albertson <ashtonalbertson@gmail.com>
2017-03-14 21:54:23 -0400

    got the random array to work

--- scripts/module/module.js
+++ scripts/module/module.js
@@ -3,6 +3,7 @@ var app = angular.module("wordModule", []);
 
 
 app.controller("wordController", function($scope){
+
   $scope.places = [
   	"Detroit", "Denver", "Atlanta", "Dallas", "Birmingham", "Houston",
   	"San Francisco", "Los Angelas", "Phenoix", "Seattle", "Portland",
@@ -13,37 +14,12 @@ app.controller("wordController", function($scope){
   	"Cincinnati", "Cleveland", "Pittsburgh", "Providence", "Austin", "Washington D.C.", "Huntsville"
   ];
 
+  $scope.randomArray = [];
+
   $scope.showWord = function(places){
     var randomCity = $scope.places[Math.floor(Math.random() * $scope.places.length)];
-  console.log(randomCity);
+    console.log(randomCity);
+    $scope.randomArray.push(randomCity);
+    console.log($scope.randomArray);
 };
 });
-
-  // $scope.showWord = function(places, $index) {
-  //   var city = $scope.places.splice($index, 1);
-  //   console.log(city);
-  // }
-
-  // $scope.showWord = function(){
-  //   $index = Math.random() * [40]
-  // }
-
-
-// $scope.getNumber = function() {
-//              $scope.num = ((Math.random() * [40]));
-//          };
-
-
-// $scope.removeItem = function(letsRemove, $index){
-//   console.log(letsRemove);
-//   console.log($index);
-// var x =$scope.items.splice($index, 1);
-// $scope.completed.push(x[0]);
-// };
-
-//
-// app.controller('inputCtrl', function($scope, redditFactory){
-//   $scope.sendName = function(subName) {
-//     console.log(subName);
-
-// <button type="button" ng-click = "sendName(subredditName)">Search For A Subreddit</button>
