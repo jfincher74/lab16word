01876d63b78a6576244a01ac5ae2439184063630
Ashton Albertson <ashtonalbertson@gmail.com>
2017-03-14 20:41:03 -0400

    working with the array

--- scripts/module/module.js
+++ scripts/module/module.js
@@ -1,11 +1,15 @@
 var app = angular.module("wordModule", []);
 
-var places = [
-	"Detroit", "Denver", "Atlanta", "Dallas", "Birmingham", "Houston",
-	"San Francisco", "Los Angelas", "Phenoix", "Seattle", "Portland",
-	"Kansas City", "Saint Louis", "New Orleans", "Chicago", "Indianapolis",
-	"Lexington", "Louisville", "New York", "Memphis", "Jackson", "Madison",
-	"Minneapolis", "Savnnah", "Tampa Bay", "Miami", "Las Vegas", "Billings",
-	"Baltimore", "Philedelphia", "Buffalo", "Raleigh", "Charleston", "Nashville",
-	"Cincinnati", "Cleveland", "Pittsburgh", "Providence", "Austin", "Washington D.C.", "Huntsville"
-]
+
+
+app.controller("wordController", function($scope){
+  $scope.places = [
+  	"Detroit", "Denver", "Atlanta", "Dallas", "Birmingham", "Houston",
+  	"San Francisco", "Los Angelas", "Phenoix", "Seattle", "Portland",
+  	"Kansas City", "Saint Louis", "New Orleans", "Chicago", "Indianapolis",
+  	"Lexington", "Louisville", "New York", "Memphis", "Jackson", "Madison",
+  	"Minneapolis", "Savnnah", "Tampa Bay", "Miami", "Las Vegas", "Billings",
+  	"Baltimore", "Philedelphia", "Buffalo", "Raleigh", "Charleston", "Nashville",
+  	"Cincinnati", "Cleveland", "Pittsburgh", "Providence", "Austin", "Washington D.C.", "Huntsville"
+  ];
+});
