564cea8eda38166d3931f4c72084123d33084fb0
Jay Fincher <fincher_jay@yahoo.com>
2017-03-15 16:56:07 -0400

    added css and updated js & html

--- scripts/module/module.js
+++ scripts/module/module.js
@@ -22,4 +22,29 @@ app.controller("wordController", function($scope){
     $scope.randomArray.push(randomCity);
     console.log($scope.randomArray);
 };
+  $scope.randomArray2 = [];
+
+  $scope.showWord2 = function(places){
+    var randomCity = $scope.places[Math.floor(Math.random() * $scope.places.length)];
+    console.log(randomCity);
+    $scope.randomArray2.push(randomCity);
+    console.log($scope.randomArray2);
+ };
+  $scope.randomArray3 = [];
+
+  $scope.showWord3 = function(places){
+    var randomCity = $scope.places[Math.floor(Math.random() * $scope.places.length)];
+    console.log(randomCity);
+    $scope.randomArray3.push(randomCity);
+    console.log($scope.randomArray3);
+
+};
+  $scope.randomArray4 = [];
+
+  $scope.showWord4 = function(places){
+    var randomCity = $scope.places[Math.floor(Math.random() * $scope.places.length)];
+    console.log(randomCity);
+    $scope.randomArray4.push(randomCity);
+    console.log($scope.randomArray4);
+};
 });
